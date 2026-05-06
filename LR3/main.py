568d77dfb6872d154a84d10ad9e2b36999be0190
Alexey <ya.glazyrin2018@yandex.ru>
2026-05-06 16:56:26 +0300

    modified plotting

--- LR3/main.py
+++ LR3/main.py
@@ -11,10 +11,17 @@ solution = Solver(G_n, G_b, J_b, A, elements)
 U_t, I_t, time = solution.solve(time_step, 1e-3, element_name)
 
 #region
-U_t = [U*1e12 for U in U_t]
-I_t = [I*1e12 for I in I_t]
 fig, ax = plt.subplots(figsize=(10, 6))
-ax.grid()
+ax.legend(fontsize=10)
+ax.minorticks_on()
+
+ax.grid(which='major', alpha=0.3)
+ax.grid(which='minor', linestyle=':', alpha=0.15)
+
+ax.set_xlabel("t, с", fontsize=14, fontname='Courier new', fontweight='bold')
+ax.set_ylabel("U, В", fontsize=14, fontname='Courier new', fontweight='bold')
+
+U_t = [U*1e12 for U in U_t]
 ax.plot(time, U_t)
 plt.tight_layout()
 plt.show()
